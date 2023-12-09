31012f243d84ed22ef3ae51444d94d5991053690
Josh <joshscina@gmail.com>
2023-12-09 18:52:37 -0500

    Set global python env for neovim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.tokyo_night_dark_float = false
+vim.g.python3_host_prog = "/usr/bin/python3"
