2c25315b377faffff86b73ca8f99bb1e40e600ad
Jscina <joshscina@gmail.com>
2023-12-28 14:18:40 -0500

    Changed colors so that variables match function argument colors

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -9,6 +9,9 @@ return {
             sidebars = "transparent",
             floats = "transparent",
           },
+          on_highlights = function(hi, c)
+            hi["@variable"] = { fg = c.yellow, style = "italic" }
+          end,
         })
       end,
     },
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,12 +4,38 @@ return {
     opts = {
       servers = {
         rust_analyzer = {
+          standalone = true,
           settings = {
             ["rust-analyzer"] = {
               diagnostics = {
                 enable = true,
                 disabled = { "unresolved-proc-macro" },
                 enableExperimental = true,
+              },
+              imports = {
+                granularity = {
+                  group = "module",
+                },
+                prefix = "self",
+              },
+              cargo = {
+                features = { "ssr" },
+                loadOutDirsFromCheck = true,
+                buildScripts = {
+                  enable = true,
+                },
+              },
+              check = {
+                features = { "ssr" },
+              },
+              -- Add clippy lints for Rust.
+              checkOnSave = {
+                features = { "ssr" },
+                command = "clippy",
+                extraArgs = { "--no-deps" },
+              },
+              procMacro = {
+                enable = false,
               },
             },
           },
