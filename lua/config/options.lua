bb1a3037220047d03867e2c994b598b96c5278ea
Jscina <joshscina@gmail.com>
2023-12-31 22:37:27 -0500

    Setup code folding options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,7 @@
 -- Add any additional options here
 vim.g.tokyo_night_dark_float = false
 vim.g.python3_host_prog = "/usr/bin/python3"
+vim.opt.foldmethod = "indent"
+vim.opt.foldnestmax = 10
+vim.opt.foldlevelstart = 1
+vim.opt.foldenable = false
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,33 +12,11 @@ return {
                 disabled = { "unresolved-proc-macro" },
                 enableExperimental = true,
               },
-              imports = {
-                granularity = {
-                  group = "module",
-                },
-                prefix = "self",
-              },
-              cargo = {
-                features = { "ssr" },
-                loadOutDirsFromCheck = true,
-                buildScripts = {
-                  enable = true,
-                },
-              },
-              check = {
-                features = { "ssr" },
-              },
-              -- Add clippy lints for Rust.
-              checkOnSave = {
-                features = { "ssr" },
-                command = "clippy",
-                extraArgs = { "--no-deps" },
-              },
-              procMacro = {
-                enable = false,
-              },
             },
           },
+        },
+        tailwindcss = {
+          filetypes_include = { "rust" },
         },
       },
     },
